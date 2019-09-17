a2db8c6ce72958b233167c4f5cec8397ae1d5efe
Lukas Seglias <lukas.seglias@gmail.com>
2019-09-17 19:09:09 +0200

    Revert "Added some test code"
    This reverts commit c4bf936c

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,8 +1,5 @@
 #include "../some/someClass.hpp"
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/nonfree/features2d.hpp>
 
 using std::cout;
 using std::endl;
@@ -15,17 +12,5 @@ int main( int argc, char** argv )
         return -1;
     }
     display(argv[1]);
-
-    const cv::Mat input = cv::imread("input.jpg", 0); //Load as grayscale
-
-    cv::Ptr<cv::SiftFeatureDetector> detector = cv::SiftFeatureDetector::create();
-    std::vector<cv::KeyPoint> keypoints;
-    detector->detect(input, keypoints);
-
-    // Add results to image and save.
-    cv::Mat output;
-    cv::drawKeypoints(input, keypoints, output);
-    cv::imwrite("sift_result.jpg", output);
-
     return 0;
 }
